99f3b122a351aeb51985dd0a81bb98c8936f10d2
unknown <gebb.freelancer@gmail.com>
2019-11-28 08:30:23 +0800

    v3

--- wp-content/plugins/gebb-first-plugin/gebb-first-plugin.php
+++ wp-content/plugins/gebb-first-plugin/gebb-first-plugin.php
@@ -54,18 +54,33 @@ defined('ABSPATH') or die('Hey, you can\t access this file!');
 class GebbzPlugin
 {
 
+    function __construct()
+    {
+        add_action('init', array($this, 'custom_post_type'));
+    }
+
     function activate()
     {
-       
+        //generated a Custom Post Type
+        $this->custom_post_type();
+
+        //flush rewrite rules
+        flush_rewrite_rules();
     }
 
     function deactivate()
     {
-        
+        //flush rewrite rules
+        flush_rewrite_rules();
     }
 
-    function uninstall()
-    { }
+   
+
+    function custom_post_type()
+    {
+
+        register_post_type('book', ['public' => true, 'label' => 'Books']);
+    }
 }
 
 
@@ -82,4 +97,3 @@ register_activation_hook(__FILE__, array($gebbzPlugin, 'activate'));
 register_deactivation_hook(__FILE__, array($gebbzPlugin, 'deactivate'));
 
 
-//uninstall
